bd453ca2f32bed4886505a9227e4a6597a178cb2
Darkar <steven.leonh2006@gmail.com>
2026-02-13 22:18:16 -0500

    comandos git

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,13 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        system.out.println("PRIMEROS COMANDOS DE GIT")
+        system.out.println("PRIMEROS COMANDOS DE GIT ")
         system.out.println("Angel Steven Leon Hernandez")
-        system.out.println("C++")
+        system.out.println("1. COMANDOS DE GIT VISTOS")
+        system.out.println("2. git add .")
+        system.out.println("3. git commit -m """)
+        system.out.println("4. git push ")
+        system.out.println("5. git switch")
         }
     }
 }
